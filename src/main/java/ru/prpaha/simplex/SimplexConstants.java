5e2321370d25d4a7b4b2c9061b9946c9f6309343
Проскурин Павел Юрьевич <p.proskurin@voskhod.ru>
2020-12-27 22:35:39 +0300

    Fix protocol

--- src/main/java/ru/prpaha/simplex/SimplexConstants.java
+++ src/main/java/ru/prpaha/simplex/SimplexConstants.java
@@ -6,7 +6,7 @@ package ru.prpaha.simplex;
 public class SimplexConstants {
 
     public static final String API_KEY_PREFIX = "ApiKey";
-    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public static final String MAIN_NET_URL = "https://backend-wallet-api.simplexcc.com";
     public static final String TEST_NET_URL = "https://sandbox.test-simplexcc.com";
 
